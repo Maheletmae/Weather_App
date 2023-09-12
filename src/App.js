fc0fdbb7a866b533dfc6634de864e477fda78ec4
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2023-09-12 08:36:51 +0200

    testing another form code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
-import Search from "./Search";
+//import Search from "./Search";
+import Testing from "./Testing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Search Engine</h1>
-        <Search />
-      </header>
+      <h1>Weather App: Search Engine</h1>
+      <br />
+      <Testing />
+      <br />
+      <hr />
+      <br />
+      <p>
+        This project was coded by <a>Mahelet Lisa Seleshi</a> and is
+        open-sourced on{" "}
+        <a
+          href="https://github.com/Maheletmae/Weather_App"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://steady-cuchufli-d72249.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Netlify
+        </a>
+        .
+      </p>
     </div>
   );
 }
---
+++ src/Testing.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function Testing() {
+  return (
+    <div className="container">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input type="search" className="form-control" />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-outline-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
