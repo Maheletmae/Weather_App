82e90254be9680f556929f28d01ec771b3ff9cfe
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2023-09-20 10:16:15 +0200

    updating forecast file

--- src/Forecast.js
+++ src/Forecast.js
@@ -4,35 +4,53 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function Forecast(props) {
-  let days = ["Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun."];
-  let date = new Date(props.data.dt * 1000);
-  let day = date.getDay();
-
-  let maxtemp = Math.round(props.data.temp.max);
-  let mintemp = Math.round(props.data.temp.min);
-  let miniIcon = props.data.weather[0].icon;
-
   let [forecast, setForecast] = useState(null);
-  let [city, setCity] = useState(null);
-
-  const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  return (
-    <div className="Forecast">
-      <ul>
-        <li>{days[day]}</li>
-        <li className="smallImages">{miniIcon}</li>
-        <li>
-          <span> `${maxtemp}°`</span>
-          <span>`${mintemp}°`</span>
-        </li>
-      </ul>
-    </div>
-  );
+  function maxTemp() {
+    let maxtemp = Math.round(props.data.temp.max);
+    return maxtemp;
+  }
+  function minTemp() {
+    let mintemp = Math.round(props.data.temp.min);
+    return mintemp;
+  }
+  function day() {
+    let days = ["Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun."];
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    return days[day];
+  }
+  function miniIcon() {
+    let Icon = props.data.weather[0].icon;
+    return Icon;
+  }
+
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <ul>
+          <li>{day()}</li>
+          <li className="smallImages">{miniIcon}</li>
+          <li>
+            <span> `${maxTemp}°`</span>
+            <span>`${minTemp}°`</span>
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
